e3026e052312fc8482adc7a21ba0c3745b48ec53
leandro <marcelleandro@outlook.com>
2026-03-17 20:16:44 -0300

    ajuste1

--- clinicaMedica/backend/src/database/connection.ts
+++ clinicaMedica/backend/src/database/connection.ts
@@ -2,7 +2,14 @@ import mongoose from 'mongoose';
 
 export async function connectDatabase() {
   try {
-    await mongoose.connect(process.env.MONGO_URI!);
+    // Adicionado para depuração:
+    console.log('Tentando conectar... URI:', process.env.MONGO_URI);
+
+    if (!process.env.MONGO_URI) {
+      throw new Error('A variável de ambiente MONGO_URI não foi definida!');
+    }
+
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Banco conectado');
   } catch (error) {
     console.log('Erro ao conectar no banco', error);
